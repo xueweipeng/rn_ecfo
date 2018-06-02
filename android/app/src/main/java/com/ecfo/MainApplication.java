55865109de48b00a46e65bc41c1a3145cd4a3dab
xueweipeng <xueweipeng-bd@ofo.com>
2018-06-02 17:51:17 +0800

    增加Android配置

--- android/app/src/main/java/com/ecfo/MainApplication.java
+++ android/app/src/main/java/com/ecfo/MainApplication.java
@@ -3,11 +3,13 @@ package com.ecfo;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.brentvatne.react.ReactVideoPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.oblador.vectoricons.VectorIconsPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -24,7 +26,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new ReactVideoPackage()
+            new SplashScreenReactPackage(),
+            new ReactVideoPackage(),
+             new VectorIconsPackage()
       );
     }
 
